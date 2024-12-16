7fe07304a315e3d5a78f6809afe487b36fee41a2
GhostASA <artemsvist1809@gmail.com>
2024-12-16 15:25:37 +0300

    git init and main dependencies

---
+++ src/main/java/com/ptoject/VkFriendsWeather/VkFriendsWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.ptoject.VkFriendsWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkFriendsWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkFriendsWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptoject/VkFriendsWeather/VkFriendsWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptoject.VkFriendsWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkFriendsWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
